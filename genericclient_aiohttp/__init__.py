01a2cb443f7492b347132c669931a4f9ff19a0a9
Flavio Curella <flavio.curella@gmail.com>
2018-04-25 13:43:19 -0500

    Bump version: 1.2.1 → 1.2.2

--- genericclient_aiohttp/__init__.py
+++ genericclient_aiohttp/__init__.py
@@ -12,7 +12,7 @@ from aiohttp.client_exceptions import ClientConnectionError, ContentTypeError
 from failsafe import Failsafe, RetryPolicy, CircuitBreaker
 
 
-_version = "1.2.1"
+_version = "1.2.2"
 __version__ = VERSION = tuple(map(int, _version.split('.')))
 
 
